6018bbe99ce4efabfe9f7eeef2f5a51d29441eb3
Jim Neuendorf <jim.neuendorf@gmx.de>
2016-08-28 00:02:23 +0200

    refactored bootstrap modal templates

--- number-of-lines.sh
+++ number-of-lines.sh
@@ -1,1 +1,2 @@
-cat $(find . -type f | sort -nr | cut -d\  -f2- | grep -v '.meteor' | grep -v '.git') | wc -l
+# cat $(find . -type f | sort -nr | cut -d\  -f2- | grep -v '.meteor' | grep -v '.git') | wc -l
+cat $(find . -type f | sort -nr | cut -d\  -f2- | grep -v '.meteor' | grep -v '.git' | grep '.js') | wc -l
